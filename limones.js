1563fe85cdb2bd3e9d0c4fb9ab93491b9e69b284
ADRIKU-app <adian02nava1997@gmail.com>
2026-09-12 17:06:33 -0500

    Agregar funcion mostrarEnSpan en utils.js y agregar setInterval

--- limones.js
+++ limones.js
@@ -10,8 +10,15 @@ let personajeX = canvas.width/2;
 let personajeY = canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE);
 let limonX=canvas.width/2;
 let limonY=5;
+let puntaje=0;
+let vidas=3;
+let velocidadCaida=200;
 
 function iniciar(){
+    /*setInterval recibe 2 parámetros:
+    1. primerParametro: function
+    2. segundoParametro: tiempo en ms*/
+    setInterval(bajarLimon,velocidadCaida);
     dibujarSuelo();
     dibujarPersonaje();
     aparecerLimon();
@@ -56,22 +63,28 @@ function dibujarLimon(){
 function bajarLimon(){
     limonY = limonY+10;
     refrescarPantalla();
-    detectarColision();
+    detectarAtrapado();
+    detectarPiso();
 
 }
 
-function detectarColision(){
+function detectarAtrapado(){
     if(limonX+ANCHO_LIMON > personajeX && limonX < personajeX + ANCHO_PERSONAJE && 
         limonY+ALTURA_LIMON > personajeY && limonY < personajeY + ALTURA_PERSONAJE
     ){
         //alert("ATRAPADO");
         aparecerLimon();
+        puntaje=puntaje+1;
+        mostrarEnSpan("txtPuntaje",puntaje);
     }
 }
 
-function probarAleatorio(){
-    let aleatorio=generarAleatorio(10,80);
-    console.log(aleatorio);
+function detectarPiso(){
+    if(limonY+ALTURA_LIMON==canvas.height-ALTURA_SUELO){
+        aparecerLimon();
+        vidas=vidas-1;
+        mostrarEnSpan("txtVidas",vidas);
+    }
 }
 
 function aparecerLimon(){
